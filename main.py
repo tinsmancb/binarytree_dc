13feb1935e6bc538c07954ab48a1c463daeca3d8
ctinsman <ctinsman@thegraysonschool.org>
2023-05-23 08:51:23 -0400

    Added validation function

--- main.py
+++ main.py
@@ -41,9 +41,9 @@ class BTree:
                 pass
             case BTree(data, left, right):
                 yield data
-                for l in left.inorder():
+                for l in left.preorder():
                     yield l
-                for r in right.inorder():
+                for r in right.preorder():
                     yield r
 
     def tolist(self):
@@ -63,22 +63,53 @@ class BTree:
             acc = f(acc, node)
         return acc
 
+    def depth(self):
+        match self:
+            case BTree(None, _, _):
+                return 0
+            case BTree(_, left, right):
+                return 1 + max(left.depth(), right.depth())
+
+    def balance(self):
+        match self:
+            case BTree(None, _, _):
+                return 0
+            case BTree(_, left, right):
+                return right.depth() - left.depth()
+
+    def validate(self):
+        match self:
+            case BTree(None, _, _):
+                pass
+            case BTree(data, left, right):
+
+                try:
+                    if left.data >= data:
+                        raise ValueError(f"Left side {left.data} is bigger than current node {data}")
+                except TypeError:
+                    pass
+
+                try:
+                    if right.data < data:
+                        raise ValueError(f"Right side {right.data} is smaller than current node {data}")
+                except TypeError:
+                    pass
+
 
 def main():
-    tree = BTree()
-    tree.insert(5)
-    tree.insert(1)
-    tree.insert(10)
-    tree.insert(-2)
+    tree_unbalanced = BTree()
+    tree_unbalanced.append([0, 1, 2, 3, 4, 5, 6])
 
-    square_tree = tree.map(lambda x: x**2)
-    print(tree.tolist())
+    tree_balanced = BTree()
+    tree_balanced.append([3, 1, 5, 0, 2, 4, 6])
 
-    print(square_tree.tolist())
+    tree_unbalanced.validate()
+    print(f"Unbalanced tree has depth {tree_unbalanced.depth()}")
+    print(f"Unbalanced tree has balance factor {tree_unbalanced.balance()}")
 
-    print(f"Sum of values is {tree.fold(lambda x,y: x+y, 0)}")
-    print(f"Largest value is {tree.fold(lambda x,y: max(x, y), -10000)}")
-    print(f"Sum of squared values is {square_tree.fold(lambda x,y:x+y, 0)}")
+    tree_balanced.validate()
+    print(f"Balanced tree has depth {tree_balanced.depth()}")
+    print(f"Balanced tree has balance factor {tree_balanced.balance()}")
 
 
 if __name__ == '__main__':
